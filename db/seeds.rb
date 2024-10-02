8eeabe95f734c55ac4778002a4048857532f408f
jpashley1 <jpashley1@gmail.com>
2024-10-01 21:54:16 -0400

    finished users and updated date format

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,25 @@
 require 'csv'
 
-file = File.join(Rails.root, "db", "todossheet.csv")
-CSV.foreach(file) do |row|
-  user_id = row[0]
-# p user_id
-  category_id = row[1]
-# p category_id
-  title = row[2]
-# p title
-  description = row[3]
-# p description
-  deadline = row[4]
-# p deadline
-  completed = row[5]
-# p completed
-  Todo.create(user_id: user_id, category_id: category_id, title: title, description: description, deadline: deadline, completed: completed)
-end
+# file = File.join(Rails.root, "db", "todossheet.csv")
+# CSV.foreach(file) do |row|
+#   user_id = row[0]
+# # p user_id
+#   category_id = row[1]
+# # p category_id
+#   title = row[2]
+# # p title
+#   description = row[3]
+# # p description
+#   deadline = row[4]
+# # p deadline
+#   completed = row[5]
+# # p completed
+#   Todo.create(user_id: user_id, category_id: category_id, title: title, description: description, deadline: deadline, completed: completed)
+# end
 
 file = File.join(Rails.root, "db", "UsersData.csv")
 CSV.foreach(file) do |row|
-  user_id = row[0]
+  name = row[0]
 # p user_id
   email = row[1]
 # p email
@@ -28,7 +28,7 @@ CSV.foreach(file) do |row|
   password_confirmation = row[3]
 # p password_confirmation
  
-  Todo.create(user_id: user_id, email: email, password: password, password_confirmation: password_confirmation)
+  User.create(name: name, email: email, password: password, password_confirmation: password_confirmation)
 end
 
 
