98f93ddf334b2c7f8150b0bab704b88aacb41d2b
jpashley1 <jpashley1@gmail.com>
2024-10-01 20:14:40 -0400

    fixed date formatting in todo controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,23 +2,57 @@ class TodosController < ApplicationController
 
 
   def index
-    render json: {message: "todo index"}
+    @todos = Todo.all
+    render :index
   end
 
   def show
-    render json: {message: "todo show"}
+    @todo = Todo.find_by(id: params[:id])
+    render :show
+  end
+
+  def parse_date(date_string)
+    Date.strptime(date_string, '%Y-%m-%d') 
   end
 
   def create
-    render json: {message: "todo create"}
+ 
+    @todo = Todo.new(
+      user_id: params[:user_id],
+      category_id: params[:category_id],
+      title: params[:title],
+      description: params[:description],
+      deadline: parse_date(params[:deadline]),
+      completed: params[:completed]
+    )
+    if @todo.save
+      render :show
+    else
+      #error handling
+      render json: { errors: @todo.errors.full_messages}, 
+      status: :unprocessable_entity
+    end
   end
 
   def update
-    render json: {message: "todo update"}
+    @todo = Todo.find_by(id: params[:id])
+
+    @todo.update(
+      user_id: params[:user_id] || @todo.user_id,
+      category_id: params[:category_id] || @todo.category_id,
+      title: params[:title] || @todo.title,
+      description: params[:description] || @todo.description,
+      deadline: parse_date(params[:deadline]) || @todo.deadline,
+      completed: params[:completed] || @todo.completed
+    )
+    @todo.save!
+    render :show
   end
 
   def destroy
-    render json: {message: "todo destroy"}
+    @todo = Todo.find_by(id: params[:id])
+    @todo.destroy
+    render json: { message: "todo has been removed" }
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-<<<<<<< HEAD
+
 
 
   get "/todos" => "todos#index"
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   post "/todos" => "todos#create"
   patch "/todos/:id" => "todos#update"
   delete "/todos/:id" => "todos#destroy"
-=======
+
   get "users" => "users#index"
   get "users/:id" => "users#show"
   patch "/users/:id" => "users#update"
@@ -22,7 +22,6 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
   
 
->>>>>>> 4b7375224a540589c559be1b0a0db682ee944665
   # Defines the root path route ("/")
   # root "posts#index"
 end
